52c5d7e367cb1485d3bec4cc022f6ac4fbdd61be
Keagen <keegenjt@gmail.com>
2024-09-17 11:05:39 -0400

    Added stuff. Not currently working.

--- os/project1/main.c
+++ os/project1/main.c
@@ -6,26 +6,37 @@
 #define READ 0
 #define WRITE 1
 
-int id;
-int circleSize = 4;
+struct message {
+  int src; // Source node ID
+  int dst; // Destination node ID
+  char* message;
+}
+
+int id = 0;
+int circleSize = 0;
 int rootPipe[2];
 int lastPipe[2];
 int nextPipe[2];
+int apple = 0;  
+
+struct message* data;
 
 void addRingNodes();
 void createNodeRing();
 
 int main() {
-  id = 0;
+  // Obtain size of node circle
+  while(circleSize <= 1) {
+    puts("Enter valid number of nodes in circle");
+    scanf("%d", &circleSize);
+    while(getchar() != '\n');
+  }
 
   createNodeRing();
-
   close(lastPipe[WRITE]);
   close(nextPipe[READ]);
 
-  printf("[%d]: [%d, %d]\n", id, lastPipe[READ], lastPipe[WRITE]);
-  printf("[%d]: [%d, %d]\n", id, nextPipe[READ], nextPipe[WRITE]);
-  sleep(1);
+  communicate();
 
   return 0;
 }
@@ -43,7 +54,9 @@ void createNodeRing() {
 
   addRingNodes();
 
-  // Finish ring connection
+  if(id == 0) {
+    apple = 1;
+  }
 }
 
 void addRingNodes() {
@@ -80,5 +93,25 @@ void addRingNodes() {
   if(pid > 0){
     // All parent processes
   }
+}
+
+void communicate() {
+  while(1) {
+    if(apple == 1) {
+      sleep(1); // TODO temp to slowdown output
+      data++;
+      write(nextPipe[WRITE], &data, sizeof(data));
+      printf("[%d] sent: %d\n", id, data);
+      apple = 0;
+    } else {
+      read(lastPipe[READ], &data, sizeof(data));
+      printf("[%d] recieved: %d\n", id, data);
+      apple = 1;
+    }
+  }
+}
+
+void intHandler(int sigNum) {
+
 }
 
