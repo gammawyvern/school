daeeb0ce874b18f3f7704c61a06f2c59248d661c
Keagen <keegenjt@gmail.com>
2024-09-18 14:48:11 -0400

    Finally got the struct and data passing

--- os/project1/message.c
+++ os/project1/message.c
@@ -12,15 +12,20 @@ void communicate() {
   while(1) {
     if(apple == 1) {
       if(data.dst == id) {
-        printf("[%d] recieved message from [%d]\n", id, data.src);
+        printf("[%d] recieved message from [%d]: %s\n", id, data.src, data.text);
+        free(data.text);
+
         data = createMessage();
       }
 
       printf("[%d] forwarding message to [%d]\n", id, data.dst);
       write(nextPipe[WRITE], &data, sizeof(struct message));
+      write(nextPipe[WRITE], data.text, data.len);
       apple = 0;
     } else {
       read(lastPipe[READ], &data, sizeof(struct message));
+      data.text = malloc(data.len);
+      read(lastPipe[READ], data.text, data.len);
       apple = 1;
     }
   }
@@ -29,8 +34,15 @@ void communicate() {
 struct message createMessage() {
   struct message msg;
 
-  printf("Enter message: ");
-  getline();
+  char* tmpText = NULL;
+  size_t bufferSize = 0;
+  printf("[%d] Enter message: ", id);
+  size_t textLen = getline(&tmpText, &bufferSize, stdin);
+
+  if(tmpText[textLen - 1] == '\n') {
+    tmpText[textLen - 1] = '\0';
+    textLen--;
+  }
 
   int dstID = -1;
   while(dstID < 0 || dstID >= circleSize) {
@@ -41,6 +53,9 @@ struct message createMessage() {
 
   msg.src = id;
   msg.dst = dstID;
+  msg.len = textLen;
+  msg.text = malloc(msg.len); 
+  strcpy(msg.text, tmpText);
 
   return msg;
 }
--- os/project1/message.h
+++ os/project1/message.h
@@ -8,8 +8,8 @@
 struct message {
   int src; // Source node ID
   int dst; // Destination node ID
-  int text;
   int len; // Length of text
+  char* text;
 };
 
 void communicate();
