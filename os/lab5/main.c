21d3ee61c1fed8af701043e70ee458ffa16065da
Keagen <keegenjt@gmail.com>
2024-09-30 12:29:35 -0400

    Added main start to lab 5

---
+++ os/lab5/main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+#define FOO 4096
+
+int main () {
+  int shmId;
+  char *sharedMemoryPtr;
+
+  shmId = shmget(IPC_PRIVATE, FOO, IPC_CREAT|S_IRUSR|S_IWUSR);
+  if(shmId < 0) {
+    perror("Unable to get shared memory\n");
+    exit(1);
+  }
+
+  sharedMemoryPtr = shmat(shmId, 0, 0);
+  if(sharedMemoryPtr == (void*)-1) {
+    perror("Unable to attach\n");
+    exit(1);
+  }
+
+  printf("Value a: %p\t Value b: %p\n", (void *)sharedMemoryPtr, (void *)sharedMemoryPtr + FOO);
+  
+  struct shmid_ds shm_info;
+  shmctl(shmId, IPC_STAT, &shm_info);
+  printf("Amount of memory allocated: %d\n", shm_info.shm_segsz);
+
+  if(shmdt(sharedMemoryPtr) < 0) {
+    perror("Unable to detach\n");
+    exit(1);
+  }
+
+  if(shmctl(shmId, IPC_RMID, 0) < 0) {
+    perror("Unable to deallocate\n");
+    exit(1);
+  }
+
+  return 0;
+}
+
