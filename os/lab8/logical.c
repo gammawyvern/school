1ffe87a264b91cd62a730d410850e57170653093
Keagen <keegenjt@gmail.com>
2024-10-30 20:03:31 -0400

    Remove wip code for 2nd part

--- os/lab8/logical.c
+++ os/lab8/logical.c
@@ -1,7 +1,9 @@
 #include <stdio.h> // Shared C libraries
-#include <stdlib.h>
+#include <stdlib.h> // Shared C libraries
 
 int main();
+long offset();
+void recurse();
 int one();
 int two();
 
@@ -12,44 +14,67 @@ int uninitialized_1; // Static data
 int uninitialized_2; // Static data
 
 int main() {
-  int stack_1;
-  int stack_2;
+  // Get relative distance from main
+  void *main_addr = (void*)&main;
 
+  printf("=== Program Memory Map ===\n\n");
+
+  // Program Text
   printf("Program Text:\n");
-  printf("main:\t\t%p\n", &main);
-  printf("function one:\t%p\n", &one);
-  printf("function two:\t%p\n", &two);
+  printf("main:\t%ld\n", offset(main_addr, &main));
+  printf("one:\t%ld\n", offset(main_addr, &one));
+  printf("two:\t%ld\n", offset(main_addr, &two));
   printf("\n");
 
+  // Initialized Data
   printf("Initialized Data:\n");
-  printf("one:\t%p\n", &initialized_1);
-  printf("two:\t%p\n", &initialized_2);
+  printf("one:\t%ld\n", offset(main_addr, &initialized_1));
+  printf("two:\t%ld\n", offset(main_addr, &initialized_2));
   printf("\n");
 
+  // Uninitialized Data
   printf("Uninitialized Data:\n");
-  printf("one:\t%p\n", &uninitialized_1);
-  printf("two:\t%p\n", &uninitialized_2);
+  printf("one:\t%ld\n", offset(main_addr, &uninitialized_1));
+  printf("two:\t%ld\n", offset(main_addr, &uninitialized_2));
   printf("\n");
 
-  void* run_time = malloc(20);
+  // Run-Time Data (Heap)
+  void* run_time_1 = malloc(20);
   void* run_time_2 = malloc(10);
-
-  printf("Run Time:\n");
-  printf("one:\t%p\n", run_time);
-  printf("two:\t%p\n", run_time_2);
+  printf("Run-Time Data (Heap):\n");
+  printf("one:\t%ld\n", offset(main_addr, &run_time_1));
+  printf("two:\t%ld\n", offset(main_addr, &run_time_2));
   printf("\n");
 
+  // Stack Data
   printf("Stack Data:\n");
-  printf("one:\t%p\n", &stack_1);
-  printf("two:\t%p\n", &stack_2);
+  recurse(5, main_addr);
   printf("\n");
 
-  printf("Shared Library Data:\n");
-  printf("one:\t%p\n", &printf);
-  printf("two:\t%p\n", &malloc);
+  // Shared Library Data
+  printf("Shared Library Functions:\n");
+  printf("one:\t%ld\n", offset(main_addr, &printf));
+  printf("two:\t%ld\n", offset(main_addr, &malloc));
   printf("\n");
+
+  free(run_time_1);
+  free(run_time_2);
 
   return 0;
+}
+
+long offset(void* main_addr, void* next_addr) {
+  return (long)(next_addr - main_addr);
+}
+
+void recurse(int num, void* last_addr) {
+  if (num <= 0) {
+    return;
+  }
+
+  int x;
+  printf("Next:\t%ld\n", offset(last_addr, &x));
+  recurse(--num, last_addr);
 }
 
 // Program text ?
@@ -63,4 +88,3 @@ int two() {
   int y = 4 * 4;
   return y;
 }
-
