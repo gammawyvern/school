ca175cd042c480fed0164d4c15dbeb6cea054bec
Keagen <keegenjt@gmail.com>
2024-11-18 22:30:46 -0500

    Added old lab10 work

--- os/lab10/other.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-void main() {
-  printf("Page size is 4096 bytes\nAllocated 1MiB of memory\nState after allocation: Committed\nMemory deallocated.\nState after deallocation: Free\n");
-}
-
